59da74144cc4382067a92ad516dd1275a2f2a835
sunrise <811706895@qq.com>
2016-05-15 14:22:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('ss');
+let a = 'a';
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+Array.from('ddd');
+console.log('ss');
+let a = 'a';
+const pai = "3.14";
+const sum = (a, b)=> a + b;
+let x = sum(1,2);
+console.log(x);
+alert('s');
